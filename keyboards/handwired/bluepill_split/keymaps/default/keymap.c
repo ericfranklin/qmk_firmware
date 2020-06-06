129533dcccd7db1e217132e701f171ad5a1a04a6
EricFranklin <ef.franklin@gmail.com>
2020-06-06 19:47:39 +0200

    Update media buttons and Second layer

--- keyboards/handwired/bluepill_split/keymaps/default/keymap.c
+++ keyboards/handwired/bluepill_split/keymaps/default/keymap.c
@@ -19,20 +19,29 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Define layer names
 #define _NORMAL 0
-#define _FNONE 1
-#define _FNTWO 2
+#define _ARROWS 1
 
 // Highly Modified by Frank
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_NORMAL] = LAYOUT_bluepill_split( \
 		/*	1			2			3			4			5			6			7			8			9 */		\
-			KC_FN0, 	KC_FN0, 	KC_FN0, 	KC_FN0, 	KC_FN0, 	KC_FN0, 	RESET, 							\
+			KC_ESC, 	KC_MEDIA_PREV_TRACK,KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,KC_AUDIO_MUTE,KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP, 							\
+
 			KC_ESC, 	KC_F1,  	KC_F2,  	KC_F3,  	KC_F4,  	KC_F5,  	KC_F6, 								\
 			KC_GRAVE, 	KC_1,   	KC_2,   	KC_3,   	KC_4,   	KC_5,   	KC_6, 								\
 			KC_TAB, 	KC_Q,   	KC_W,   	KC_E,   	KC_R,   	KC_T,   	KC_Y, 		KC_DELETE,					\
 			KC_CAPS, 	KC_A,   	KC_S,   	KC_D,   	KC_F,   	KC_G,   	KC_H,		KC_END,					\
 			KC_LSFT, 	KC_NUBS,	KC_Z,   	KC_X,   	KC_C,   	KC_V,   	KC_B,		KC_PGDOWN,					\
-			KC_LCTL, 	KC_LGUI,	KC_LALT,      						KC_SPC,		KC_FN0,		KC_FN0)
+			KC_LCTL, 	KC_LGUI,	KC_LALT,      						KC_SPC,		MO(_ARROWS),KC_FN0),
+	[_ARROWS] = LAYOUT_bluepill_split( \
+		/*	1			2			3			4			5			6			7			8			9 */		\
+			_______, 	_______, 	_______, 	_______, 	_______, 	_______, 	RESET, 							\
+			_______, 	_______,  	_______,  	_______,  	_______,  	_______,  	_______, 								\
+			_______, 	_______,   	_______,   	_______,   	_______,   	_______,   	_______, 								\
+			_______, 	_______,   	_______,   	KC_UP,   	_______,   	_______,   	_______, 	_______,					\
+			_______, 	_______,   	KC_LEFT,   	KC_DOWN,   	KC_RIGHT,  	_______,   	_______,	_______,					\
+			_______, 	_______,	_______,   	_______,   	_______,   	_______,   	_______,	_______,					\
+			_______, 	_______,	_______,      						_______,	_______,	_______)
 };
 
 /* Layer based ilumination, just binary */
